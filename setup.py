d9a57677cd0bf96841b993e38846eea17f46a375
Paul perso <70315271+paularnaud2@users.noreply.github.com>
2021-04-03 11:06:14 +0200

    setup install_requires

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+from shutil import copyfile
 from setuptools import find_packages, setup
 
 setup(packages=find_packages())
+copyfile('pytools/conf_main_default.py', 'pytools/_conf_main.py')
+copyfile('pytools/conf_oracle_default.py', 'pytools/_conf_oracle.py')
