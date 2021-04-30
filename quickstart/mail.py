91fb863555dbda05a19e7f2f517680fe142f9512
paularnaud2 <70315271+paularnaud2@users.noreply.github.com>
2021-04-30 11:10:49 +0200

    Update mail.py

--- quickstart/mail.py
+++ quickstart/mail.py
@@ -1,7 +1,9 @@
 # In order for the mail function to be worinkg, you have to initialise a mail
 # folder by running the 'init_mail' function. This will create a folder in the
-# MAIL_DIR defined in 'conf.py' ('mails/' by default). If you want to use the
-# gmail or no_auth functions, you have to set a confidential.txt file.
+# MAIL_DIR defined in 'conf.py' ('mails/' by default).
+#
+# If you want to use the gmail or no_auth functions, you have to set a
+# confidential.txt file.
 # This file must be saved at the path defined in the pytools/conf.py file
 # (CFI_PATH) using the example provided in the initialised folder.
 #
@@ -17,15 +19,17 @@
 # recipients. For your test, just let one line with your infos.
 
 from datetime import date
-from pytools.mail import gmail
-from pytools.mail import init_mail
+import pytools.mail as mail
 
 # First run ini_mail and set a confidential.txt file from the example in 'mails/'
-init_mail()
+# mail.init_mail()
 
 # Then run the gmail function to send a test mail (pointing to mails/test/)
 var_dict = {
     'DATE': str(date.today()),
     'NAME': 'dear tester',
 }
-gmail('test', '[Test] Python mail', var_dict=var_dict)
+
+# mail.gmail('test', '[Test] Python mail', var_dict=var_dict)
+# mail.no_auth('test', '[Test] Python mail', var_dict=var_dict)
+mail.outlook('test', '[Test] Python mail', var_dict=var_dict)
