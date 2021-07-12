c5d657d2e41026f25861ed2c4a2775bfd93c4420
Paul Arnaud <paularnaud2@gmail.com>
2021-07-12 11:04:06 +0200

    Added like_list and like_dict

--- tests/test_1_utils.py
+++ tests/test_1_utils.py
@@ -34,11 +34,35 @@ def get_duration():
     u.log_print()
 
 
+def like():
+
+    u.log_print("Test of like functions", dashes=100)
+
+    s = '2 test ok?'
+    assert u.like(s, 'test')
+    u.log("like simple ok")
+
+    m = u.like(s, '2 * ok?')
+    assert m.group(1) == 'test'
+    u.log("like m ok")
+
+    lst = ['1', 'test']
+    assert u.like_list(s, lst)
+    u.log("like_list ok")
+
+    dct = {'1': 'a', '2': 'test'}
+    assert u.like_dict(s, dct) == '2'
+    u.log("like_dict ok")
+    u.log_print()
+
+
 def test_utils():
     init_PT()
     msg_box()
     get_duration()
+    like()
     u.check_log(cl.CO)
+    u.log_print()
 
     pt.cfg.FILES_DIR = back
     u.g.init_PT(True)
