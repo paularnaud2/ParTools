4ee1500a197a920c28dfc9ea85349037949aae3e
Paul Arnaud <paularnaud2@gmail.com>
2021-11-12 08:51:21 +0100

    v1.0.9 OK

--- deploy/PTconf.py
+++
@@ -1,2 +0,0 @@
-# This directory contains input, output, log, and temporary files folders.
-FILES_DIR = 'PT1/'
--- partools/changelog.py
+++ partools/changelog.py
@@ -1,3 +1,4 @@
-VERSION = '1.0.8'  # conf overwriting
+VERSION = '1.0.9'  # more robust init_mail
+# VERSION = '1.0.8'  # conf overwriting
 # VERSION = '1.0.7'  # Added like_list and like_dict
 # VERSION = '1.0.6'  # get_duration_string, list_to_dict strip
